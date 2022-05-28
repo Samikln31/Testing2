4abe155f82b3f7cf8bb69f3f697211d5622fe0b0
Kunal Lawand <kunallavand@gmail.com>
2022-05-28 20:39:58 +0530

    dot added

--- Test/src/Pack1/Hello.java
+++ Test/src/Pack1/Hello.java
@@ -6,6 +6,6 @@ public class Hello
 	public static void main(String args[])	
 	{
 			System.out.println("Hello hiii  ");
-			System.out.println("m back...");
+			System.out.println("m back....");
 	}
 }
